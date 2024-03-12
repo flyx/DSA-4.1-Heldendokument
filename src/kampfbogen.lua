5338e60f40c1464f9bbd45b3eee3d6db725e2904
David Foerster <david.foerster@informatik.hu-berlin.de>
2024-03-12 15:14:18 +0100

    kampfbogen.energieleiste(): use math.round(), fix other issue
    - Use a *local* variable to store intermediary results.
    - Emit "&" and the integral value with a single tex.sprint() since the latter
      can't result in special characters.

--- src/kampfbogen.lua
+++ src/kampfbogen.lua
@@ -405,13 +405,12 @@ end
 
 function kampfbogen.energieleiste(label, val)
   tex.sprint(-2, label)
-  num = tonumber(val)
-  if num == nil then
+  val = tonumber(val)
+  if val == nil then
     tex.sprint("&&&&")
   else
     for i=1,4 do
-      tex.sprint("&")
-      tex.sprint(-2, common.round(num/i))
+      tex.sprint("&", math.round(val / i))
     end
   end
   tex.sprint("&")
