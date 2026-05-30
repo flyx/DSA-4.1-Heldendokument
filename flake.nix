9476030404079400cf120ab4fae648784e91d78c
Felix Krause <contact@flyx.org>
2026-05-30 16:31:50 +0200

    updated nixpkgs to 26.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Heldendokument-Generator und Webinterface dazu";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     utils.url = "github:numtide/flake-utils";
     nix-filter.url = "github:numtide/nix-filter";
     wds-handouts = {
